f41e75fda8a59577e6e62a1fcf2505c168898522
NicolasTetreaultIMD <NICOLASTETREAULT@cmail.carleton.ca>
2024-03-20 14:35:22 -0400

    adding puzzles to mission control

--- src/worlds/TTime_MissionControl/js/telegraph-interaction.js
+++ src/worlds/TTime_MissionControl/js/telegraph-interaction.js
@@ -15,6 +15,8 @@ const MorseDelay = {
 //Manages the telegraph interactions
 AFRAME.registerComponent('telegraph-interaction', {
     schema: {
+        angle:{type:'int', default:0},
+        scale:{type:'float', default:1},
     },
     init() {
         const CONTEXT_AF = this;
@@ -29,8 +31,9 @@ AFRAME.registerComponent('telegraph-interaction', {
         //Light element
         CONTEXT_AF.morseLight = document.createElement('a-entity');
         CONTEXT_AF.morseLight.setAttribute('id', 'morseLight');
-        CONTEXT_AF.morseLight.setAttribute('position', {x:0,y:0.58,z:0});
-        CONTEXT_AF.morseLight.setAttribute('geometry', {primitive:'cylinder', height:0.2, radius:0.1});
+        CONTEXT_AF.morseLight.setAttribute('position', {x:0.12,y:0,z:0.08});
+        CONTEXT_AF.morseLight.setAttribute('geometry', {primitive:'cylinder', height:0.3, radius:0.1});
+        CONTEXT_AF.morseLight.setAttribute('scale', {x:CONTEXT_AF.data.scale,y:CONTEXT_AF.data.scale,z:CONTEXT_AF.data.scale});
         CONTEXT_AF.morseLight.setAttribute('material', {color:'#316934', emissive:'#00ff00', emissiveIntensity:0});
 
         CONTEXT_AF.el.appendChild(CONTEXT_AF.morseLight);
@@ -40,6 +43,8 @@ AFRAME.registerComponent('telegraph-interaction', {
         let telegraphElements = document.createElement('a-entity');
         telegraphElements.setAttribute('id', 'telegraphElements');
         telegraphElements.setAttribute('circles-interactive-visible', 'true');
+        telegraphElements.setAttribute('rotation', {x:0,y:CONTEXT_AF.data.angle,z:0});
+        telegraphElements.setAttribute('scale', {x:CONTEXT_AF.data.scale,y:CONTEXT_AF.data.scale,z:CONTEXT_AF.data.scale});
         
         CONTEXT_AF.telegraphElements = telegraphElements;
 
